3bd623e92dc8e9aa05795428dff7a1a3fb18c556
Ajaykumaryadav-Aj <ajaykumarsiwan182@gmail.com>
2023-09-06 17:47:03 +0530

    add safearea

--- lib/screens/contact_box.dart
+++ lib/screens/contact_box.dart
@@ -33,123 +33,125 @@ class ChatMessagebox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.teal,
-        title: const Text(
-          'Select contact',
-          style: TextStyle(color: Colors.white),
-        ),
-        actions: const [
-          Padding(
-            padding: EdgeInsets.all(8.0),
-            child: Icon(
-              Icons.search,
-              color: Colors.white,
-            ),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.teal,
+          title: const Text(
+            'Select contact',
+            style: TextStyle(color: Colors.white),
           ),
-          Padding(
-            padding: EdgeInsets.all(8.0),
-            child: Icon(
-              Icons.more_vert,
-              color: Colors.white,
-            ),
-          ),
-        ],
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            ListView.builder(
-              physics: const NeverScrollableScrollPhysics(),
-              shrinkWrap: true,
-              itemCount: contactlist.length,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  leading: CircleAvatar(
-                    backgroundColor: Colors.teal,
-                    child: Icon(
-                      contactlist[index]['icon'],
-                      color: Colors.white,
-                    ),
-                  ),
-                  title: Text(
-                    contactlist[index]['New'],
-                    style: const TextStyle(
-                        fontWeight: FontWeight.bold, fontSize: 18),
-                  ),
-                  trailing: Icon(
-                    contactlist[index]['sideicon'],
-                    size: 30,
-                  ),
-                );
-              },
-            ),
-            const Padding(
-              padding: EdgeInsets.fromLTRB(0, 20, 215, 0),
-              child: Text(
-                'Contacts on WhatsApp',
-                style: TextStyle(fontSize: 16),
+          actions: const [
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Icon(
+                Icons.search,
+                color: Colors.white,
               ),
             ),
-            ListView.builder(
-              physics: const NeverScrollableScrollPhysics(),
-              shrinkWrap: true,
-              itemCount: contactprsnlist.length,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  leading: CircleAvatar(
-                    backgroundImage: AssetImage(chatimage[index]['image']),
-                  ),
-                  title: Text(
-                    contactprsnlist[index]['name'],
-                    style: const TextStyle(
-                        fontWeight: FontWeight.bold, fontSize: 18),
-                  ),
-                  subtitle: Text(
-                    contactprsnlist[index]['message'],
-                    style: const TextStyle(fontSize: 16),
-                  ),
-                  trailing: Text(
-                    contactprsnlist[index]['sidetext'],
-                    style: const TextStyle(fontSize: 16, color: Colors.teal),
-                  ),
-                );
-              },
-            ),
-            const Padding(
-              padding: EdgeInsets.fromLTRB(0, 10, 250, 0),
-              child: Text(
-                'Invite to WhatsApp',
-                style: TextStyle(fontSize: 16),
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Icon(
+                Icons.more_vert,
+                color: Colors.white,
               ),
             ),
-            ListView.builder(
-              itemCount: invitelist.length,
-              shrinkWrap: true,
-              physics: const NeverScrollableScrollPhysics(),
-              scrollDirection: Axis.vertical,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  leading: CircleAvatar(
-                    child: Icon(
-                      invitelist[index]['icon'],
-                      color: Colors.white,
-                    ),
-                  ),
-                  title: Text(
-                    invitelist[index]['name'],
-                    style: const TextStyle(
-                        fontWeight: FontWeight.bold, fontSize: 17),
-                  ),
-                  trailing: Text(
-                    invitelist[index]['sidetext'],
-                    style: const TextStyle(fontSize: 15, color: Colors.teal),
-                  ),
-                );
-              },
-            )
           ],
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              ListView.builder(
+                physics: const NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
+                itemCount: contactlist.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    leading: CircleAvatar(
+                      backgroundColor: Colors.teal,
+                      child: Icon(
+                        contactlist[index]['icon'],
+                        color: Colors.white,
+                      ),
+                    ),
+                    title: Text(
+                      contactlist[index]['New'],
+                      style: const TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 18),
+                    ),
+                    trailing: Icon(
+                      contactlist[index]['sideicon'],
+                      size: 30,
+                    ),
+                  );
+                },
+              ),
+              const Padding(
+                padding: EdgeInsets.fromLTRB(0, 20, 215, 0),
+                child: Text(
+                  'Contacts on WhatsApp',
+                  style: TextStyle(fontSize: 16),
+                ),
+              ),
+              ListView.builder(
+                physics: const NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
+                itemCount: contactprsnlist.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: AssetImage(chatimage[index]['image']),
+                    ),
+                    title: Text(
+                      contactprsnlist[index]['name'],
+                      style: const TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 18),
+                    ),
+                    subtitle: Text(
+                      contactprsnlist[index]['message'],
+                      style: const TextStyle(fontSize: 16),
+                    ),
+                    trailing: Text(
+                      contactprsnlist[index]['sidetext'],
+                      style: const TextStyle(fontSize: 16, color: Colors.teal),
+                    ),
+                  );
+                },
+              ),
+              const Padding(
+                padding: EdgeInsets.fromLTRB(0, 10, 250, 0),
+                child: Text(
+                  'Invite to WhatsApp',
+                  style: TextStyle(fontSize: 16),
+                ),
+              ),
+              ListView.builder(
+                itemCount: invitelist.length,
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                scrollDirection: Axis.vertical,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    leading: CircleAvatar(
+                      child: Icon(
+                        invitelist[index]['icon'],
+                        color: Colors.white,
+                      ),
+                    ),
+                    title: Text(
+                      invitelist[index]['name'],
+                      style: const TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 17),
+                    ),
+                    trailing: Text(
+                      invitelist[index]['sidetext'],
+                      style: const TextStyle(fontSize: 15, color: Colors.teal),
+                    ),
+                  );
+                },
+              )
+            ],
+          ),
         ),
       ),
     );
